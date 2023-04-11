bbb169a9c2e79de951b114812a202b0abadc92da
Erlanda Zakaria <erlandazakaria@gmail.com>
2023-04-11 20:48:04 +0700

    [FEATURE] Add Github Workflows

--- src/Pages/Excel.tsx
+++ src/Pages/Excel.tsx
@@ -8,7 +8,7 @@ export default function Excel() {
     <>
       <Container maxWidth={false} sx={{width: "100%", height: "720px", padding: "0px !important", zIndex: 2}}>
         <Grid width="100%" height="100%" sx={{backgroundColor: "#e2f1fd", textAlign: "center"}}>
-          <Button sx={{mt: 5}} variant="contained" target="_blank" href={`${process.env.REACT_APP_SERVER_URL}/asl-message`}>
+          <Button sx={{mt: 5}} variant="contained" target="_blank" href={`${process.env.REACT_APP_SERVER_URL}/asl/message`}>
             Generate Excel
           </Button>
         </Grid>
